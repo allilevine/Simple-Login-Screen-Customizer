0f5cd77f1ea0d1b6ff603ed2b22bfa0bffa83e59
Allison Levine <allison@allison-levines-macbook-pro.local>
2014-05-25 11:56:39 -0400

    Updated plugin to version 1.0.1

--- simple-login-screen-customizer.php
+++ simple-login-screen-customizer.php
@@ -10,14 +10,14 @@
  * Plugin Name:       Simple Login Screen Customizer
  * Plugin URI:        https://github.com/allilevine/Simple-Login-Screen-Customizer
  * Description:       Choose a link color and logo for the login screen.  You can also choose rollover and button colors if you'd like. 
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Allison Levine
- * Author URI:        https://allisonlevine.com
+ * Author URI:        https://github.com/allilevine
  * Text Domain:       plugin-name-locale
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/allilevine/<repo>
+ * GitHub Plugin URI: https://github.com/allilevine/Simple-Login-Screen-Customizer
  */
 
 // If this file is called directly, abort.
